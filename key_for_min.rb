5d36788f186515e79e9c6a015b827c6d01799d1d
--add <andrewksmoker@gmail.com>
2020-10-22 15:42:41 -0400

    commit tree 3439646829f745dbd27185493183969faafdba5c

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    if name_hash == {}
+    nil
+    else
+    integers = []
+        name_hash.collect do |name, number| 
+        integers << number
+            integers = integers.sort do |a, b|
+                a <=> b
+            end
+        end
+        name_hash.each do |name, number|
+            if number == integers[0]
+            return name
+            end
+        end
+    end
 end
